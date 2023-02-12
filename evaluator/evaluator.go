e10f37be1f37556c802e6c6d35918d55b410a535
Naoya Yamashita <conao3@gmail.com>
2023-02-12 21:19:53 +0900

    implement apply function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -4,6 +4,32 @@ import (
 	"github.com/conao3/go-glisp/types"
 )
 
+
+func extendedFunctionEnv(arg_syms_ *types.Cons, args_ *types.Cons, env *types.Environment) *types.Environment {
+	extended_env := types.NewEnclosedEnvironment(env)
+
+	for {
+		arg_sym_ := arg_syms_.Car
+		arg_ := args_.Car
+
+		arg_sym := arg_sym_.(*types.Symbol)
+		arg := Eval(arg_, env)
+
+		extended_env.SetValue(arg_sym.Name, arg)
+
+		if arg_syms_.Cdr == &types.NIL && args_.Cdr == &types.NIL {
+			break
+		}
+		if arg_syms_.Cdr == &types.NIL || args_.Cdr == &types.NIL {
+			panic("argument length mismatch")
+		}
+		arg_syms_ = arg_syms_.Cdr.(*types.Cons)
+		args_ = args_.Cdr.(*types.Cons)
+	}
+
+	return extended_env
+}
+
 func Eval(expr types.Expr, env *types.Environment) types.Expr {
 	switch expr := expr.(type) {
 	case *types.Symbol:
@@ -65,6 +91,8 @@ func Eval(expr types.Expr, env *types.Environment) types.Expr {
 				}
 			case "quote":
 				return expr.Cdr.(*types.Cons).Car
+			case "lambda":
+				return expr
 			case "set":
 				sym_ := expr.Cdr.(*types.Cons).Car
 				val_ := expr.Cdr.(*types.Cons).Cdr.(*types.Cons).Car
@@ -80,7 +108,21 @@ func Eval(expr types.Expr, env *types.Environment) types.Expr {
 				env.SetFunction(sym.Name, val)
 				return val
 			default:
-				panic("not implemented")
+				fn, ok := env.GetFunction(car.Name)
+				if !ok {
+					panic("undefined function")
+				}
+
+				if fn.(*types.Cons).Car.(*types.Symbol).Name != "lambda" {
+					panic("this is not a function")
+				}
+				arg_syms := fn.(*types.Cons).Cdr.(*types.Cons).Car.(*types.Cons)
+				body := fn.(*types.Cons).Cdr.(*types.Cons).Cdr.(*types.Cons).Car
+				args := expr.Cdr.(*types.Cons)
+
+				extended_env := extendedFunctionEnv(arg_syms, args, env)
+
+				return Eval(body, extended_env)
 			}
 		case *types.Cons:
 			switch caar := car.Car.(type) {
@@ -89,28 +131,9 @@ func Eval(expr types.Expr, env *types.Environment) types.Expr {
 				case "lambda":
 					arg_syms := car.Cdr.(*types.Cons).Car.(*types.Cons)
 					body := car.Cdr.(*types.Cons).Cdr.(*types.Cons).Car
-					args_ := expr.Cdr.(*types.Cons)
+					args := expr.Cdr.(*types.Cons)
 
-					extended_env := types.NewEnclosedEnvironment(env)
-
-					for {
-						arg_sym_ := arg_syms.Car
-						arg_ := args_.Car
-
-						arg_sym := arg_sym_.(*types.Symbol)
-						arg := Eval(arg_, env)
-
-						extended_env.SetValue(arg_sym.Name, arg)
-
-						if arg_syms.Cdr == &types.NIL && args_.Cdr == &types.NIL {
-							break
-						}
-						if arg_syms.Cdr == &types.NIL || args_.Cdr == &types.NIL {
-							panic("argument length mismatch")
-						}
-						arg_syms = arg_syms.Cdr.(*types.Cons)
-						args_ = args_.Cdr.(*types.Cons)
-					}
+					extended_env := extendedFunctionEnv(arg_syms, args, env)
 
 					return Eval(body, extended_env)
 				default:
